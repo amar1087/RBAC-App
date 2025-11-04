55e7a697f035c89737ef363169496e4703475aff
Amarjeet Kaur <amar1087@gmail.com>
2025-11-04 09:51:26 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { Login } from './pages/login/login';
+import { Roles } from './pages/roles/roles';
+import { Dashboard } from './pages/dashboard/dashboard';    
+import { Users } from './pages/users/users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'roles', component: Roles, canActivate: [authGuard] },
+    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+    { path: 'users', component: Users, canActivate: [authGuard] },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '**', redirectTo: 'login', pathMatch: 'full' },
+];
